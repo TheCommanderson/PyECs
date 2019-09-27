8c15e535719e8ed5174df1ad6c78ecdb0727c24b
TheCommanderson <spencerc.anderson@hotmail.com>
2019-09-26 23:51:06 -0500

    Finished finitePoints()

--- ECBase.py
+++ ECBase.py
@@ -1,12 +1,39 @@
+'''
+FUNCTION finitePoints
+arguments - C (ECCurve)
+returns - points (list[ECPoints]), a list of all the finite points on the curve C,
+or an empty list if no finite points exist on the graph
+'''
+from math import sqrt
+from numpy import roots, isreal
+
+def finitePoints(C):
+    assert(isinstance(C, ECCurve)), "Argument must be of type ECCurve"
+    points = set()
+    y_list = [0]
+    for i in range(1, round(sqrt(abs(C.disc)))):
+        if not abs(C.disc) % i:
+            y_list += [i, C.disc/i]
+
+    for y in y_list:
+        x_vals = roots([1, C.a, C.b, C.c-y**2])
+        for x in x_vals:
+            if isreal(x):
+                x = round(x.real, 8) # rounding to 8 because of some small computational rounding error due to python
+                if x.is_integer():
+                    points.add(ECPoint(C, x, y))
+                    points.add(ECPoint(C, x, -y))
+    return list(points)
+
 class ECCurve:
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
         self.c = c
-
+        self.disc = (-4*(a**3)*c) + (a**2*b**2) + (18*a*b*c) - (4*(b**3)) - (27*c**2)
     def __eq__(self, g2):
         return (self.a == g2.a) and (self.b == g2.b) and (self.c == g2.c)
-        
+
 class ECPoint:
     def __init__(self, graph, x, y):
         assert(isinstance(x, int) or isinstance(x, float)), "x must be a real number"
@@ -21,7 +48,9 @@ class ECPoint:
     ne
     neg
     add
+    sub
     str
+    hash - this object is hashable
     '''
     def __eq__(self, p2):
         return (self.x == p2.x) and (self.y == p2.y) and (self.graph == p2.graph)
@@ -43,6 +72,10 @@ class ECPoint:
 
         x3 = lbda**2 - g.a - self.x - p2.x
         y3 = (-lbda*x3)-nu
-        return ECPoint(g, round(x3, 7), round(y3, 7))
+        return ECPoint(g, round(x3, 8), round(y3, 8)) # round to 8 because of small computational error from python
+    def __sub__(self, p2):
+        return self + -p2
     def __str__(self):
         return '(' + str(self.x) + ', ' + str(self.y) + ')'
+    def __hash__(self):
+        return (53*hash(self.x)) + (53*hash(self.y))
